6d14be65b34c533c8d85cea5b74f49c7d1d3e3b3
buffard <samuelwebber19@gmail.com>
2018-11-02 13:27:33 -0500

    Initial setup

--- src/lib/grunt/browserify.js
+++ src/lib/grunt/browserify.js
@@ -2,7 +2,7 @@ module.exports = {
     options: {
         transform: [
             [
-                'babelify',
+                "babelify",
                 {
                     "presets": [
                         [
@@ -20,8 +20,8 @@ module.exports = {
             debug: true
         }
     },
-    dailyjournal: {
+    app: {
         src: ["../scripts/main.js"],
-        dest: "../../public/hellogoodbye.js"
+        dest: "../../public/bundle.js"
     }
 }
