dd8200a0271654fbf2e3d82c035330c5c113c78a
buffard <samuelwebber19@gmail.com>
2018-11-02 15:52:43 -0500

    not working

---
+++ src/scripts/contactCollection.js
@@ -0,0 +1,24 @@
+// const localApi = "  http://localhost:8088/contacts" 
+
+// // fetch(localApi)
+// //   .then((contacts) => database.json())
+// //   .then((parsedFoods) => {
+// //     let foodList = document.querySelector(".foodList")
+// //     parsedFoods.forEach((food) => {
+// //       foodList.innerHTML += `
+// //       <div>
+// //         <h1>${food.name} </h1>
+// //         <p>${food.type} </p>
+// //         <p>${food.ethnicity} </p>
+// //       </div>
+// //         `
+// //     });
+// //   })
+
+//   fetch("localApi", { // Replace "url" with your API's URL
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(legoToSave)
+// })
---
+++ src/scripts/contactForm.js
@@ -0,0 +1,12 @@
+// export default 
+//   document.querySelector("#btn").addEventListener("click", event => {
+//     const name = document.querySelector("#id").value
+  
+//     // Once you have collected all the values, build your data structure
+//     const person = {
+//         name: value,
+//         phone: value,
+//         address: value,
+//     }
+//   })
+
